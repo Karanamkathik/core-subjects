c92b81fd0a2cd672d5dd42f0aa163aa0e4cc79d1
karanamkathik <karanamkarthik2002@gmail.com>
2025-03-24 12:07:16 +0530

    first commit

---
+++ OOPS/Topics.cpp
@@ -0,0 +1,40 @@
+/*
+  -- class and object       
+  -- constructor and destruter
+  -- Abstraction
+  -- Encapsulation
+  -- Inheritance
+  -- Polymorphism
+
+*/
+   /*
+        OOP's is  object orieanted programming . 
+        it is a programming pradigim that uses objects to model real world problems.
+        Normal procedual programming uses the functions .
+        oops involes in creating objects that contains data and functions
+   */
+   
+
+  #include<iostream>
+  using namespace std;
+
+
+    
+   class Car{
+    public:
+    string brand;
+    string owner;
+   };  // ; is compulsory to tell compiler that class  decleration is completed.
+
+   int main(){
+    Car myCar;
+    myCar.brand = "Toyota";  
+    myCar.owner = "karthik";
+    return 0;
+   }
+
+ // class  --
+
+ //objct --
+
+ // access modifires --
